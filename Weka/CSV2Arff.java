a6c8a1511a5982d91c20e6347d7ffb2163316918
Abigail Johnson <abigailjohnson@Abigails-MacBook-Pro.local>
2020-03-22 13:29:25 -0400

    adding an updated CSV2Arff file

--- Weka/CSV2Arff.java
+++ Weka/CSV2Arff.java
@@ -1,50 +1,27 @@
-import weka.*;
-
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 
 import java.io.File;
-import java.io.IOException;
 
 public class CSV2Arff {
-  
-/**
-   * constructor takes 2 arguments:
-   * CSV input file (string of filename)
-   * ARFF output file (string of filename)
+  /**
+   * takes 2 arguments:
+   * - CSV input file
+   * - ARFF output file
    */
-	String sourceCSV;
-	String finalArff;
-	CSVLoader loader;
-	Instances data;
-	ArffSaver saver;
-	 
-	 public CSV2Arff(String csv, String arff) throws IOException {
-		 this.sourceCSV = csv;
-		 this.finalArff = arff;
-		 this.loader = new CSVLoader();
-		 loader.setSource(new File(sourceCSV));
-		 this.data = loader.getDataSet();
-		 this.saver = new ArffSaver();
-		 
-	 }
-	 
-	 
-	 /*
-	  * writes and saves the contents to the csv to an arff format 
-	  */
-	 public void saveArff() throws IOException {
-		 saver.setInstances(data);
-		 saver.setFile(new File(finalArff));
-		 saver.writeBatch();
-		 System.out.println(".arff file has been created.");
-	 }
-	
   public static void main(String[] args) throws Exception {
-    
-CSV2Arff test = new CSV2Arff("cleanedTestData.csv", "cleanedTest.arff");
-test.saveArff();
+    // load CSV
+    CSVLoader loader = new CSVLoader();
+    loader.setSource(new File("normalizedDTM.csv"));
+    Instances data = loader.getDataSet();
 
+    // save ARFF
+    ArffSaver saver = new ArffSaver();
+    saver.setInstances(data);
+    saver.setFile(new File("normalizedDTM.arff"));
+    saver.setDestination(new File("normalizedDTM.arff"));
+    saver.writeBatch();
+    System.out.println("arff file has been created.");
   }
 }
